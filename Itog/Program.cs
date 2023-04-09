d46f8c27416f126f1ccc8895e08a2270d36682c8
Mikhail Kamishov <pi_ff@mail.ru>
2023-04-09 21:45:12 +0300

    задал массив

--- Itog/Program.cs
+++ Itog/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello,  World!");
+﻿
+string[] array =
+{
+    "f",
+    "ah",
+    "cth",
+    "abnd",
+    "eojwe",
+    "p"
+};
