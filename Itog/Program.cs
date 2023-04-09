dcd6652d51d83b3542c0f2d79bdd5007ee14792e
Mikhail Kamishov <pi_ff@mail.ru>
2023-04-09 21:46:42 +0300

    написал метод выводящий массив на консоль

--- Itog/Program.cs
+++ Itog/Program.cs
@@ -13,6 +13,14 @@
     }
     return result;
 }
+void ShowArray (string[] array)
+{
+    for(int i = 0;i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
 
 string[] array =
 {
