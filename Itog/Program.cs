75be2a2e72fc9f6195e84eb9f823589ddc23fcb9
Mikhail Kamishov <pi_ff@mail.ru>
2023-04-09 21:46:08 +0300

    создал метод для создание нового массива выполняющего условие задачи

--- Itog/Program.cs
+++ Itog/Program.cs
@@ -1,4 +1,19 @@
-﻿
+﻿string[] Massiv (string[] array)
+{
+    string[] result = new string[array.Length];
+    int realSize = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string str = array[i];
+        if (str.Length <= 3)
+        {
+            result[realSize] = array[i];
+            realSize++;
+        }
+    }
+    return result;
+}
+
 string[] array =
 {
     "f",
